d686c2c14325a01a8eedc6fe66455ef34a16a1b3
Filip M <filip.moszczynski@onet.pl>
2020-10-24 11:55:02 +0200

    >D4.0 - MenuDto

---
+++ src/main/java/com/filipmoszczynski/blog/menu/MenuDto.java
@@ -0,0 +1,14 @@
+package com.filipmoszczynski.blog.menu;
+
+public class MenuDto {
+
+    private String name;
+
+    public MenuDto(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/filipmoszczynski/blog/menu/MenuService.java
@@ -0,0 +1,12 @@
+package com.filipmoszczynski.blog.menu;
+
+import java.util.List;
+
+public class MenuService {
+
+    public List<MenuDto> getMenu(){
+        return List.of(
+
+        );
+    }
+}
