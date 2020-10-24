f1744a7aaad33802ae6011b0ce113c6640d8c513
Filip M <filip.moszczynski@onet.pl>
2020-10-24 14:16:27 +0200

    > D4.0 - BlogRestController (new controller - rest api)

---
+++ src/main/java/com/filipmoszczynski/blog/BlogRestController.java
@@ -0,0 +1,34 @@
+package com.filipmoszczynski.blog;
+
+import com.filipmoszczynski.blog.menu.MenuDto;
+import com.filipmoszczynski.blog.menu.MenuService;
+import com.filipmoszczynski.blog.post.PostDto;
+import com.filipmoszczynski.blog.post.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BlogRestController {
+
+    @Autowired
+    public BlogRestController(MenuService menuService, PostService postService) {
+        this.menuService = menuService;
+        this.postService = postService;
+    }
+
+    private MenuService menuService;
+    private PostService postService;
+
+    @GetMapping("/api/blog/menu")
+    public List<MenuDto> getMenu() {
+        return menuService.getMenu();
+    }
+
+    @GetMapping("/api/blog/posts")
+    public List<PostDto> getPosts() {
+        return postService.getPosts();
+    }
+}
